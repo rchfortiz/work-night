af111c3cd18cc0d645ff2b016f90c39b58550d50
rchfortiz <rchfortiz@gmail.com>
2024-02-02 18:34:19 +0800

    Create wrapper property for camera X offset

--- game/scrolling_camera.py
+++ game/scrolling_camera.py
@@ -4,9 +4,10 @@ import pyray
 class ScrollingCamera:
     def __init__(self, zoom: float = 1.1) -> None:
         self.cam = pyray.Camera2D(pyray.vector2_zero(), pyray.vector2_zero(), 0.0, zoom)
+        self.x_offset = 0.0
 
     def update(self) -> None:
-        pass
+        self.cam.offset.x = self.x_offset
 
     def begin(self) -> None:
         pyray.begin_mode_2d(self.cam)
