f0b9e1170a3511e5894ae2eab9c4ac2364bb3fdd
rchfortiz <rchfortiz@gmail.com>
2024-03-08 21:03:43 +0800

    Generate 3 random words for typing game

--- game/typing_game.py
+++ game/typing_game.py
@@ -11,33 +11,33 @@ class TypingGame:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.current_word = ""
-        self.current_typing_word = ""
+        self.current_text = ""
+        self.current_typing_text = ""
         self.current_typing_index = 0
         self.can_continue_typing = True
 
         self._word_list = load_text_list_asset("words")
 
-        self.choose_random_word()
+        self.choose_random_text()
 
-    def choose_random_word(self):
-        self.current_word = random.choice(self._word_list)
+    def choose_random_text(self):
+        self.current_text = " ".join(random.sample(self._word_list, 3))
 
     def update(self):
-        if not self.current_word or not self.can_continue_typing:
+        if not self.current_text or not self.can_continue_typing:
             return
 
-        letter = self.current_word[self.current_typing_index]
+        letter = self.current_text[self.current_typing_index]
         if is_key_pressed(ord(letter.upper())):
             self.current_typing_index += 1
-            self.current_typing_word = self.current_word[: self.current_typing_index]
+            self.current_typing_text = self.current_text[: self.current_typing_index]
 
-        if self.current_typing_word == self.current_word:
-            # Finished the word
-            self.choose_random_word()
-            self.current_typing_word = ""
+        if self.current_typing_text == self.current_text:
+            # Finished the text
+            self.choose_random_text()
+            self.current_typing_text = ""
             self.current_typing_index = 0
 
     def draw(self):
-        draw_text(self.current_word, self.x, self.y, 30, TRANSLUCENT_BLACK)
-        draw_text(self.current_typing_word, self.x, self.y, 30, BLACK)
+        draw_text(self.current_text, self.x, self.y, 24, TRANSLUCENT_BLACK)
+        draw_text(self.current_typing_text, self.x, self.y, 24, BLACK)
